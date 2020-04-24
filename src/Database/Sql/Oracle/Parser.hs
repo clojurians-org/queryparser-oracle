ac26f64bb6c89c9b6a92a04b338f35a3295e4df2
larluo <larluo@clojurians.org>
2020-04-24 18:25:10 +0800

    init parser

--- src/Database/Sql/Oracle/Parser.hs
+++ src/Database/Sql/Oracle/Parser.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 module Database.Sql.Oracle.Parser where
 
 import Database.Sql.Position
@@ -8,7 +9,9 @@ import Database.Sql.Type
 
 import Database.Sql.Oracle.Scanner (tokenize)
 import Database.Sql.Oracle.Parser.Internal (Parser, ParserScope(..), selectTableAliases)
-import Database.Sql.Oracle.Type (OracleProcedureStatement)
+import Database.Sql.Oracle.Type
+--  ( OracleStatement(..)
+--  , InsertStatement(..))
 
 
 import qualified Data.Text.Lazy as T
@@ -17,19 +20,42 @@ import Control.Monad.Reader (Reader, runReader)
 
 import qualified Text.Parsec as P
 import qualified Text.Parsec.Pos as P
+import qualified Database.Sql.Oracle.Parser.Token as Tok
 
 
 emptyParserScope :: ParserScope
 emptyParserScope = ParserScope { selectTableAliases = Nothing }
 
-statementParser :: Parser (OracleProcedureStatement RawNames Range)
-statementParser = P.choice
-  [ undefined
-  ]
 
-parse :: T.Text -> Either P.ParseError (OracleProcedureStatement RawNames Range)
+parse :: T.Text -> Either P.ParseError (OracleStatement RawNames Range)
 parse = flip runReader emptyParserScope . P.runParserT statementParser 0 "-" . tokenize
 
-parseManyAll :: T.Text -> Either P.ParseError [OracleProcedureStatement RawNames Range]
+parseManyAll :: T.Text -> Either P.ParseError [OracleStatement RawNames Range]
 parseManyAll text = runReader (P.runParserT (P.many1 statementParser <* P.eof) 0 "-"  . tokenize $ text) emptyParserScope
 
+
+statementParser :: Parser (OracleStatement RawNames Range)
+statementParser = P.choice
+  [ P.try $ OracleInsertStmt <$> insertStatementP
+  ]
+
+insertStatementP :: Parser (InsertStatement Range)
+insertStatementP = do
+  _ <- Tok.insertP
+  insertStatementHint <- return Nothing
+  insertStatementUnion <- P.choice
+    [ InsertSingle <$> insertSingleP
+    , InsertMulti <$> insertMultiP
+    ]
+  return $ InsertStatement {..}
+
+insertSingleP :: Parser (SingleTableInsert Range)
+insertSingleP = do
+  undefined
+
+insertIntoClauseP :: Parser (InsertIntoClause Range)
+insertIntoClauseP = do
+  undefined
+
+insertMultiP :: Parser (MultiTableInsert Range)
+insertMultiP = undefined
